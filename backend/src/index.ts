af84df69f1fd82c451dde88f45bb56c7f446db34
Sumedh <sumedhmhaske310503@gmail.com>
2025-08-22 21:10:44 +0530

    Added middleware for authentication

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
@@ -9,6 +9,20 @@ const app = new Hono<{
     JWT_SECRET: string;
   };
 }>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("Authorization") || "";
+  const token = header.split(" ")[1];
+
+  const res = await verify(token, c.env.JWT_SECRET);
+
+  if (res.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "Unauthorized" });
+  }
+});
 
 app.get("/api/v1/blog/:id", (c) => {
   return c.text("Hello Hono");
@@ -56,7 +70,7 @@ app.post("/api/v1/signin", async (c) => {
   });
 
   if (!user) {
-    return c.status(403);
+    c.status(403);
     return c.json({ error: "user not found" });
   }
 
